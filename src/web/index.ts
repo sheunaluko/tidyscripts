9fd03500ee162ece0cc6fe1deba1df706517b3ab
Sheun Aluko <alukosheun@gmail.com>
2020-08-08 19:26:28 -0700

    completed hyperloop http_json | sattsys node prepped

--- src/web/index.ts
+++ src/web/index.ts
@@ -7,7 +7,11 @@ import * as cryptocurrency from "./cryptocurrency/index.ts"
 
 import * as wikidata from "./wikidata/index.ts" 
 
+import * as hyperloop from "./hyperloop/index.ts" 
+import * as apis from "./apis/index.ts" 
 
 export {util,
 	wikidata,
+	hyperloop, 
+	apis, 
 	cryptocurrency } 
--- src/web/util/index.ts
+++ src/web/util/index.ts
@@ -44,4 +44,17 @@ export function is_mobile() {
 }
 
 
+// from https://stackoverflow.com/questions/105034/how-to-create-guid-uuid => 
+export function uuid() {
+    var buf = new Uint32Array(4);
+    window.crypto.getRandomValues(buf);
+    var idx = -1;
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        idx++;
+        var r = (buf[idx>>3] >> ((idx%8)*4))&15;
+        var v = c == 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+    });
+};
+
 
