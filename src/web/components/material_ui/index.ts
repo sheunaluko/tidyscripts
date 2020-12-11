e2cf93353eb8a2af0c3badeb9f1662aaff59d748
Sheun Aluko <alukosheun@gmail.com>
2020-12-10 20:43:52 -0500

    +react components, switched to yalc, react as peer dep

--- src/web/components/material_ui/index.ts
+++ src/web/components/material_ui/index.ts
@@ -1,8 +1,10 @@
 
 
 import HtmlWidget from "./HtmlWidget"
+import VoicePanel from "./VoicePanel" 
 
 
 export { 
     HtmlWidget , 
+    VoicePanel , 
 } 
---
+++ src/web/components/material_ui/list.tsx
@@ -0,0 +1,135 @@
+
+import Box from '@material-ui/core/Box';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import InfoIcon from '@material-ui/icons/Info';
+
+import Container from '@material-ui/core/Container';
+
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import TextField from '@material-ui/core/TextField';
+
+import Input from '@material-ui/core/Input';
+import FilledInput from '@material-ui/core/FilledInput';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import Icon from '@material-ui/core/Icon';
+
+import SpeedDial from '@material-ui/lab/SpeedDial';
+import SpeedDialIcon from '@material-ui/lab/SpeedDialIcon';
+import SpeedDialAction from '@material-ui/lab/SpeedDialAction';
+
+
+
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+
+import Chip from '@material-ui/core/Chip';
+import Avatar from '@material-ui/core/Avatar';
+import FaceIcon from '@material-ui/icons/Face';
+import DoneIcon from '@material-ui/icons/Done';
+
+
+import Link from '@material-ui/core/Link';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+import Switch from '@material-ui/core/Switch';
+import Radio from '@material-ui/core/Radio';
+
+import Checkbox from '@material-ui/core/Checkbox';
+import Card from '@material-ui/core/Card';
+
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import BuildIcon from '@material-ui/icons/Build';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import Drawer from '@material-ui/core/Drawer';
+
+
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+
+export { 
+    Box, 
+    AppBar, 
+    Card,
+    Drawer, 
+    SpeedDial,
+    SpeedDialIcon,
+    SpeedDialAction,
+    Toolbar, 
+    NavigateNextIcon, 
+    Typography,
+    FormControlLabel,
+    Radio, 
+    IconButton,
+    List,
+    ListItem, 
+    ListItemIcon, 
+    ListItemText, 
+    Divider, 
+    BuildIcon,
+    Button,       
+    AddCircleOutlineIcon,
+    Breadcrumbs, 
+    Switch, 
+    Checkbox, 
+    MenuIcon,
+    Icon, 
+    Tabs, 
+    Tab, 
+    Accordion,
+    AccordionSummary, 
+    AccordionDetails,
+    CircularProgress,
+    ExpandMoreIcon, 
+    Container, 
+    Grid, 
+    
+    
+    InfoIcon, 
+    Link, 
+    Paper,
+    Autocomplete, 
+    TextField, 
+    Input, 
+    FilledInput,
+    OutlinedInput,
+    InputLabel,
+    Chip,
+    FaceIcon, 
+    DoneIcon,
+    Avatar,
+    InputAdornment,
+    FormHelperText, 
+    FormControl, 
+    Visibility, 
+    VisibilityOff, 
+} 
