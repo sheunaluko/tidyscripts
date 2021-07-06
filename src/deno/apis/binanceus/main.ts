b928ea4bac242aef8f73bd4e49508906cf2c5c5b
Sheun Aluko <alukosheun@gmail.com>
2021-07-05 19:29:01 -0500

    +dollar cost averaging bot v1 (binance us)

--- src/deno/apis/binanceus/main.ts
+++ src/deno/apis/binanceus/main.ts
@@ -9,8 +9,7 @@ import * as io from "../../io.ts"
 import {hmac} from "../../base_imports.ts" 
 import * as cutil from "../../../common/util/index.ts" 
 
-
-let log = cutil.Logger("bus_main") 
+let log = (x:any) => null // cutil.Logger("bus_main") 
 
 let base_ep = "https://api.binance.us" 
 let account_ep = base_ep + "/api/v3/account" 
@@ -40,13 +39,45 @@ export async function get_account_info() {
 	headers: { 'X-MBX-APIKEY' : `${apik}` } , 
     })
     
-    console.log(res) 
+    //console.log(res) 
     
     let json = await res.json() 
     
-    console.log(json) 
+    return json 
+}
+
+
+export async function execute_order(o : string) {
+    // hmac
+    let sig = hmac('sha256', apis, o , 'utf8', 'hex')
+    // create the req body 
+    let body = `${o}&signature=${sig}` 
+    //order ep 
+    let ep = base_ep + "/api/v3/order" 
+    // do req 
+    log(`using body: ${body}`)
+    // -- 
+    let res = await fetch(ep, { 
+	method : 'POST', 
+	headers: { 'X-MBX-APIKEY' : `${apik}` } , 
+	body : body
+    })
+    //console.log(res) 
+    let json = await res.json() 
+    return json     
 } 
 
+export async function usd_market_buy(base  :string, amount : number) {
+    let o = `symbol=${base+"USD"}&side=BUY&type=MARKET&quoteOrderQty=${amount}&recvWindow=5000&timestamp=${Date.now()}`    
+    return execute_order(o)
+} 
+
+
+export async function free_usd_balance() {
+    let info = await get_account_info() 
+    let x = info.balances.filter( (y:any) => y.asset == "USD" )[0].free   
+    return Number(x)
+} 
 
 
 
