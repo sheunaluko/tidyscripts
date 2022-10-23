af7b23b0cb52e27c32f95b40ca0096d889219f6d
Sheun Aluko <sheunaluko@Sheuns-MacBook-Pro.local>
2022-10-23 01:11:49 -0500

    ++radiopaedia case api deployed to tidyscripts

--- packages/ts_node/src/utils.ts
+++ packages/ts_node/src/utils.ts
@@ -1,9 +1,9 @@
 import * as http from "./http" 
-import {JSDOM} from 'jsdom' 
+import {JSDOM} from 'jsdom'
+import get_pixels from "get-pixels" ;
+import {logger,fp} from "tidyscripts_common" ; 
 
-export function log() {
-    console.log("!") 
-} 
+const log = logger.get_logger({id : "utils"}) ; 
 
 /**
  * Retrives the html from a url using fetch and converts it into a DOM object using JSDOM
@@ -16,6 +16,24 @@ export async function get_dom(url : string) {
 }
 
 /**
- * 
- * 
+ * Retrieves pixel values from a remote image source 
+ * Wrapper around the get_pixels lib 
  */
+export async function pixels(url : string) {
+  // - 
+  var p = new Promise( (res :any, rej :any) => {
+    // - 
+    get_pixels(url, function(err, pixels) {
+      if(err) {
+	rej("Bad image path")
+	return
+      }
+      log(`got pixels from ${url}`)
+      res(pixels) 
+    })
+    // - 
+  })
+
+  return p 
+} 
+
