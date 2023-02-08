d8886c0ff56913fe5f44abb63829d2bdde432b99
Sheun Aluko <sheunaluko@Sheuns-MBP.lan>
2023-02-08 09:57:05 -0600

    clean

--- packages/ts_common/src/index.js
+++
@@ -1,8 +0,0 @@
-import * as fp from './fp';
-import * as logger from './logger';
-import * as R from 'ramda';
-import * as trading from './trading/index';
-import * as web3 from './web3/index';
-import * as util from './util/index';
-import * as asnc from "./async";
-export { fp, logger, R, trading, web3, util, asnc, };
