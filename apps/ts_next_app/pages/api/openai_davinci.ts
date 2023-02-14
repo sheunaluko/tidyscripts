69e8c2806510260e36c9792828d2e7e9bc7f5d89
Sheun Aluko <sheunaluko@Sheuns-MBP.lan>
2023-02-13 23:28:27 -0600

    openai api fix

--- apps/ts_next_app/pages/api/openai_davinci.ts
+++ apps/ts_next_app/pages/api/openai_davinci.ts
@@ -12,10 +12,25 @@ type Data = {
   text: any , 
 }
 
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
 
   var params = req.query ; 
   var {max_tokens,prompt} = params ; 
