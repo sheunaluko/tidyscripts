1144b5b7a1605b8f03295f57c214cdb1f85859d0
sheunaluko <alukosheun@gmail.com>
2024-01-25 10:54:38 -0600

    implemented tsc compilation for local scripts; + langchain scripts

--- bin/repl_setup.ts
+++ bin/repl_setup.ts
@@ -2,6 +2,8 @@
 import repl from  "repl"
 import node from "../packages/ts_node/dist/index"
 import common from "../packages/ts_common/dist/index" ;
+import * as local from "./local/index" 
+
 
 declare var global : any ;
 const R = common.R ;
@@ -13,11 +15,13 @@ let welcome_msg = `
 Welcome to the Tidyscripts REPL (Read Eval Print Loop)
 What exciting journey awaits you?
 `
+
 async function init_repl() { 
 
     if (!global.common) { global.common = common };
     if (!global.node  ) { global.node   = node   };
-    if (!global.R     ) { global.R      = R      }; 
+    if (!global.R     ) { global.R      = R      };
+    if (!global.local ) { global.local  = local  };     
 
     log(welcome_msg)
     
@@ -32,5 +36,6 @@ export {
     common,
     get_logger , 
     R,
-    init_repl , 
+    init_repl ,
+    local, 
 } 
